5e300f315089069418f06e25e87371f511bbb65d
NasuThanks <63646014+NasuThanks@users.noreply.github.com>
2020-04-14 21:39:13 +0900

    add mul5 function (#5)
    Co-authored-by: nanigasi_san <nanigasi.py@gmail.com>

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+
+int mul5(int a);
 int sub5(int a);
 int main()
 {
     printf("Hello world\n");
     // fuck
     return 0;
+}
+
+int mul5(int a){
+    return a*5;
 }
 
 int sub5(int a)
