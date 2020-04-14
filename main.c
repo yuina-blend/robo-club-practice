986d56c37586b836cdea463a130fcd3024f9a1bc
cootekun <63646018+cootekun@users.noreply.github.com>
2020-04-14 22:00:06 +0900

    update a function "add" (#8)

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 
 int div5(int a);
-int add5(int a);
+int add(int a,int b);
 int mul5(int a);
 int sub5(int a);
 
@@ -20,7 +20,7 @@ int div5(int a)
 
 int add5(int a)
 {
-    return a + 5;
+    return a + b;
 }
 
 int mul5(int a){
